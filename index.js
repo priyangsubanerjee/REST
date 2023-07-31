98db3c508aa8e988a36650ecdfea159f7749f800
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-08-01 00:51:24 +0530

    routes

--- index.js
+++ index.js
@@ -13,8 +13,47 @@ app.get("/", (req, res) => {
   res.json({
     message: "Hello World",
     secret: `${process.env.SECRET_KEY} --  Loaded from .env file / environment variable`,
-    routes: ["/create", "/read", "/update", "/delete"],
+    GET_routes: ["/create", "/read", "/update", "/delete"],
+    POST_routes: ["/protected"],
   });
+});
+
+app.get("/create", (req, res) => {
+  res.json({
+    message: "Create route",
+  });
+});
+
+app.get("/read", (req, res) => {
+  res.json({
+    message: "Read route",
+  });
+});
+
+app.get("/update", (req, res) => {
+  res.json({
+    message: "Update route",
+  });
+});
+
+app.get("/delete", (req, res) => {
+  res.json({
+    message: "Delete route",
+  });
+});
+
+app.post("/protected", (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json({
+      message: "Authorization header required",
+    });
+  } else {
+    return res.status(200).json({
+      message: "Protected route",
+      secret: `${process.env.SECRET_KEY} --  Loaded from .env file / environment variable`,
+      headers: req.headers,
+    });
+  }
 });
 
 app.listen(port, () => {
